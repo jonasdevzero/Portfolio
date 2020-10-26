4151662aa57d5d0a273ba88cab5685e6771a6b7d
jonasdevzero <jonasdevzero@gmail.com>
2020-10-25 21:47:25 -0300

    Starting the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <h1>Portifólio</h1>
+    </div>
+  );
+}
+
+export default App;
