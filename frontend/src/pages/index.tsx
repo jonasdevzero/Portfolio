9302c82fede3ca7156d72a459f849e866ebfe66b
jonasdevzero <jonasdevzero@gmail.com>
2020-10-27 18:27:47 -0300

    Adding Home page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,20 @@
 import Head from 'next/head'
 
+import Header from '../components/Header'
+
+import {
+  Container,
+  Content,
+  Inner,
+  Title,
+  Subtitle,
+  Text,
+  Button,
+  Social
+} from '../styles/pages/Home'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub'
+
 export default function Home() {
   return (
     <div>
@@ -7,9 +22,28 @@ export default function Home() {
         <title>Portfolio</title>
       </Head>
 
-      <main > 
-        <h1>Portfolio</h1>
-      </main>
+      <Container>
+            <Header />
+
+            <Content>
+                <Inner>
+                    <Subtitle>Hello</Subtitle>
+                    <Title>I'm Jonas</Title>
+                    <Text>A self-taught of web programming</Text>
+
+                    <Button>Know more</Button>
+
+                    <Social>
+                        <a href="https://www.linkedin.com/in/jonas-de-oliveira-0561961ab/" rel="noopener noreferrer" target="_blank">
+                            <LinkedInIcon />
+                        </a>
+                        <a href="https://github.com/jonasdevzero" rel="noopener noreferrer" target="_blank">
+                            <GitHubIcon />
+                        </a>
+                    </Social>
+                </Inner>
+            </Content>
+        </Container>
     </div>
   )
 }
