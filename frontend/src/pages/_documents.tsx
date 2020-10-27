1717be9f1848ddb8a6a3dbe20b1eeb1d011ba045
jonasdevzero <jonasdevzero@gmail.com>
2020-10-27 17:40:20 -0300

    Adding some dependencies and fix vulnerabilities

--- frontend/src/pages/_documents.tsx
+++ frontend/src/pages/_documents.tsx
@@ -1,4 +1,5 @@
-import Document, { DocumentInitialProps, DocumentContext } from 'next/document'
+import React from 'react'
+import Document, { DocumentInitialProps, DocumentContext, Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -26,5 +27,20 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet='utf-8' />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
