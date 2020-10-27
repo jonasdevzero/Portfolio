cffac964218f0285d7e9e3ce7ccf4c458dcca933
jonasdevzero <jonasdevzero@gmail.com>
2020-10-27 16:40:09 -0300

    Adding typescript and config the project

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Portfolio</title>
+      </Head>
+
+      <main > 
+        <h1>Portfolio</h1>
+      </main>
+    </div>
+  )
+}
