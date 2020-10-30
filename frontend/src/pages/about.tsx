bfc1a0ea535fb2283f5b7566e1ec7c824b733892
jonasdevzero <jonasdevzero@gmail.com>
2020-10-30 14:01:23 -0300

    Building the scope of about page

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -6,8 +6,17 @@ import { Header } from '../components'
 import {
     Container,
     Content,
-    Inner
+    Inner,
+    Card,
+    ImageContainer,
+    Image,
+    Info,
+    Title,
+    Text,
+    Button
 } from '../styles/pages/About'
+
+import profile from '../assets/img1.jpg'
 
 function About() {
     return (
@@ -21,7 +30,24 @@ function About() {
 
                 <Content>
                     <Inner>
-                        <h1>About</h1>
+                        <Card>
+                            <ImageContainer>
+                                <Image src={profile} />
+                            </ImageContainer>
+
+                            <Info>
+                                <Title>Jonas de Oliveira</Title>
+
+                                <Text>
+                                    Hello, I am 17 years old and I love programming and I discovered this passion after 
+                                    seeing a little of this wonderful world that is programming and making my first hello 
+                                    world. I've been studying web programming for a few months, using javascript programming 
+                                    language on the frontend with Reactjs and the backend with nodejs.
+                                </Text>
+
+                                <Button>See my skills</Button>
+                            </Info>
+                        </Card>
                     </Inner>
                 </Content>
             </Container>
--- frontend/src/styles/pages/About.ts
+++ frontend/src/styles/pages/About.ts
@@ -5,9 +5,7 @@ export const Container = styled.div`
     flex-direction: column;
     align-items: center;
 
-    height: 100%;
-
-    overflow: hidden;
+    height: 100vh;
 `;
 
 export const Content = styled.main`
@@ -17,7 +15,6 @@ export const Content = styled.main`
 
     width: 100vw;
     margin-top: 6rem;
-    margin-bottom: 6rem;
 `;
 
 export const Inner = styled.section`
@@ -25,7 +22,126 @@ export const Inner = styled.section`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    position: relative;
 
-    height: calc(100vh - 12rem);
+    height: 100%;
+`;
+
+export const Card = styled.div`
+    display: flex;
+    align-items: center;
+    flex: 1;
+    
+    width: 100%;
+
+    @media (max-width: 900px) {
+        flex-direction: column;
+        padding-top: 1rem;
+    };
+`;
+
+export const ImageContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    flex: .4;
+    height: 40rem;
+
+    @media (max-width: 1080px) {
+        flex: .3;
+    };
+
+    @media (max-width: 900px) {
+        margin-bottom: 2rem;
+    };
+`;
+
+export const Image = styled.img`
+    height: 100%;
+    object-fit: contain;
+
+    @media (max-width: 900px) {
+        width: 100%;
+        height: auto;
+        object-fit: none;
+    };
+`;
+
+export const Info = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    flex: .6;
+    
+    height: 40rem;
+    padding: 2rem;
+
+    @media (max-width: 1080px) {
+        flex: .7;
+    };
+
+    @media (max-width: 900px) {
+        flex: 1;
+        padding: 0 3rem;
+        height: 100%;
+    };
+`;
+
+export const Title = styled.h1`
+    font-size: 3.8rem;
+    font-weight: 600;
+    text-align: start;
+
+    width: 100%;
+    margin-bottom: 1.5rem;
+
+    @media (max-width: 930px) {
+        font-size: 3rem;
+    };
+`;
+
+export const SubTitle = styled.h2`
+    font-size: 2rem;
+    font-weight: 500;
+
+    margin-left: 1rem;
+`;
+
+export const Text = styled.p`
+    font-size: 1.6rem;
+    font-weight: 500;
+    letter-spacing: .1rem;
+
+    margin-bottom: 1rem;
+
+    @media (max-width: 930px) {
+        font-size: 1.8rem;
+    };
+`;
+
+export const Button = styled.button`
+    height: 5rem;
+    width: 15rem;
+    background-color: #191818;
+    color: #FFF;
+    transition: background-color .3s ease; 
+
+    margin: 2.5rem 0;
+    border: none;
+    border-radius: .4rem;
+    outline: none;
+    cursor: pointer;
+
+    font-size: 1.4rem;
+    font-weight: 600;
+
+    &:hover {
+        background-color: #000;
+    };
+
+    @media (max-width: 400px) {
+        height: 4.5rem;
+        width: 12rem;
+
+        font-size: 1.2rem;
+    };
 `;
