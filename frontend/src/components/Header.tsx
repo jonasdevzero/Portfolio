575a849b7cab338186565fb58ccfd88440579120
jonasdevzero <jonasdevzero@gmail.com>
2020-11-02 13:57:34 -0300

    refactor

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import {
     Container,
     Navigation,
+    StyledLink,
     Dropside,
     DropsideButton,
     Button,
 } from '../styles/components/Header'
 import MenuIcon from '@material-ui/icons/Menu'
+import CloseIcon from '@material-ui/icons/Close'
 
-function Header() {
+function Header({ local }) {
     const [showDropside, setShowDropside] = useState(false)
     const [scroll, setScroll] = useState(false)
     const router = useRouter()
@@ -21,7 +23,7 @@ function Header() {
             if (window.scrollY > 0) {
                 setScroll(true)
                 return
-            } 
+            }
             setScroll(false)
         })
     }, [])
@@ -35,31 +37,69 @@ function Header() {
         setShowDropside(true)
     }
 
-    function goTo(url: string) {
-        setShowDropside(false)
+    function goTo(href: string) {
         setTimeout(() => {
-            router.push(url)
-        }, 430)
+            setShowDropside(false)
+        }, 300)
+
+        setTimeout(() => {
+            router.push(href)
+        }, 800)
     }
 
     return (
         <Container className={scroll ? 'scroll' : ''}>
             <Navigation>
-                <Link href='/'>Home</Link>
-                <Link href='/about'>About</Link>
-                <Link href='/portfolio'>Portfolio</Link>
-                <Link href='/contac'>Contac</Link>
+                <Link href='/'>
+                    <StyledLink className={local}>Home</StyledLink>
+                </Link>
+                <Link href='/about'>
+                    <StyledLink className={local}>About</StyledLink>
+                </Link>
+                <Link href='/portfolio'>
+                    <StyledLink className={local}>Portfolio</StyledLink>
+                </Link>
+                <Link href='/contact'>
+                    <StyledLink className={local}>Contac</StyledLink>
+                </Link>
+                <div className='header-animation'></div>
             </Navigation>
 
             <Dropside show={showDropside}>
-                <DropsideButton onClick={() => goTo('/')}>Home</DropsideButton>
-                <DropsideButton onClick={() => goTo('/about')}>About</DropsideButton>
-                <DropsideButton onClick={() => goTo('/portfolio')}>Portfolio</DropsideButton>
-                <DropsideButton onClick={() => goTo('/contac')}>Contact</DropsideButton>
+                <DropsideButton 
+                    className={local} 
+                    onClick={() => goTo('/')}
+                >
+                    Home
+                </DropsideButton>
+                <DropsideButton 
+                    className={local} 
+                    onClick={() => goTo('/about')}
+                >
+                    About
+                </DropsideButton>
+                <DropsideButton 
+                    className={local} 
+                    onClick={() => goTo('/portfolio')}
+                >
+                    portfolio
+                </DropsideButton>
+                <DropsideButton 
+                    className={local} 
+                    onClick={() => goTo('/contact')}
+                >
+                    Contact
+                </DropsideButton>
+
+                <div className="dropside-animation"></div>
             </Dropside>
 
-            <Button onClick={toggleDropside}>
-                <MenuIcon fontSize='large' />
+            <Button onClick={toggleDropside} show={showDropside}>
+                {showDropside ? 
+                    <CloseIcon />
+                    :
+                    <MenuIcon />
+                }
             </Button>
         </Container>
     );
