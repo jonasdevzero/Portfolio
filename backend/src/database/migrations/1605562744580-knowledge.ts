0ba4bfa788c8aab44609a4e27a61fb6ae3852002
jonasdevzero <jonasdevzero@gmail.com>
2020-11-17 22:21:37 -0300

    feat: restarting backend

---
+++ backend/src/database/migrations/1605562744580-knowledge.ts
@@ -0,0 +1,46 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class knowledge1605562744580 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'knowledge',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                },
+                {
+                    name: 'description',
+                    type: 'text',
+                },
+                {
+                    name: 'image_url',
+                    type: 'text',
+                },
+                {
+                    name: 'about_link',
+                    type: 'text',
+                },
+                {
+                    name: 'created_at',
+                    type: 'varchar',
+                    default: Date.now(),
+                },
+            ],
+        }));
+    };
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('knowledge');
+    };
+
+};
---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+import './database/connection';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ message: 'ok' }));
+
+app.listen(PORT, () => console.log('server running: http://localhost:3001'));
