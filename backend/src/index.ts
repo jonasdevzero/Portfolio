13bfe1afadeaffe0487db38858fcf7f7562f45e6
jonasdevzero <jonasdevzero@gmail.com>
2020-11-18 12:01:32 -0300

    feat: Adding routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,13 @@ import express from 'express';
 import cors from 'cors';
 
 import './database/connection';
+import routes from './routes';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => res.json({ message: 'ok' }));
+app.use(routes);
 
 app.listen(PORT, () => console.log('server running: http://localhost:3001'));
