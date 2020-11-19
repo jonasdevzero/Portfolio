930de7decdb710930a0ba1716d7097cd21d4e2e4
jonasdevzero <jonasdevzero@gmail.com>
2020-11-18 23:18:04 -0300

    feat: adding models to project and image

---
+++ backend/src/models/Project.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn } from 'typeorm';
+import Image from '../models/Image';
+
+@Entity('projects')
+export default class Project {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    source_link: string;
+
+    @Column()
+    about_link: string;
+
+    @OneToMany(_ => Image, image => image.project, {
+        cascade: ['insert', 'update', 'remove'],
+    })
+    @JoinColumn({ name: 'project_id' })
+    images: Image[];
+};
