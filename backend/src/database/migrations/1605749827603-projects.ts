46c2b2cade43ad135d9bc3e1bc7996b0fa1a0da7
jonasdevzero <jonasdevzero@gmail.com>
2020-11-18 22:59:57 -0300

    feat: adding migrations - projects and images

---
+++ backend/src/database/migrations/1605749827603-projects.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table,  } from "typeorm";
+
+export class projects1605749827603 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'projects',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment',
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                },
+                {
+                    name: 'description',
+                    type: 'text',
+                },
+                {
+                    name: 'source_link',
+                    type: 'text',
+                },
+                {
+                    name: 'about_link',
+                    type: 'text'
+                },
+            ],
+        }));
+    };
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('projects');
+    };
+
+};
