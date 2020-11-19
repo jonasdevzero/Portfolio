f76608c20e6c6a2ab187b2f67bebf72d7472cd8f
jonasdevzero <jonasdevzero@gmail.com>
2020-11-19 11:27:48 -0300

    feat: Adding project controller and routes

---
+++ backend/src/controllers/ProjectController.ts
@@ -0,0 +1,114 @@
+import { Request, Response } from 'express';
+import Project from '../models/Project';
+import Image from '../models/Image';
+import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
+
+export default {
+    async index(req: Request, res: Response) {
+        try {
+            const projectRepository = getRepository(Project);
+            const project = await projectRepository.find({ relations: ['images'] });
+
+            return res.status(200).json({ project });
+        } catch (err) {
+            console.log('Error on (index) [project] -> ', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        };
+    },
+
+    async show(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+
+            const projectRepository = getRepository(Project);
+            const project = await projectRepository.findOneOrFail(id, { relations: ['images'] })
+                .catch(err => res.status(404).json({ error: 'Incorrect id' }));
+
+            return res.status(200).json({ project });
+        } catch (err) {
+            console.log('Error on (show) [project] -> ', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    },
+
+    async create(req: Request, res: Response) {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required(),
+                description: Yup.string().required(),
+                source_link: Yup.string().required(),
+                about_link: Yup.string().required(),
+                banner_image: Yup.string().required(),
+                images: Yup.array(Yup.object().shape({
+                    path: Yup.string().required(),
+                })),
+            });
+            await schema.validate(req.body, { abortEarly: false })
+                .catch(err => res.status(400).json({
+                    error: {
+                        message: `field(s) empty`,
+                        fields: err.inner.map((field: any) => field.path),
+                    },
+                }));
+
+            const projectRepository = getRepository(Project);
+
+            const project = projectRepository.create(req.body);
+            await projectRepository.save(project);
+
+            return res.status(201).json({ project });
+        } catch (err) {
+            console.log('Error on (create) [project] -> ', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        };
+    },
+
+    async update(req: Request, res: Response) {
+        if (!req.params.id) return res.status(400).json({ error: 'id missing' });
+
+        try {
+            const { id } = req.params;
+
+            if (req.body.images) {
+                const images =  req.body.images;
+                const imageRepository = getRepository(Image);
+
+                await imageRepository.delete({ project: {  id: Number(id) } });
+
+                images.map(async (image: { path: string }) => {
+                    const newImage = imageRepository.create({ project: { id: Number(id) }, path: image.path });
+                    await imageRepository.save(newImage);
+                });                
+                
+                delete req.body.images;
+            };
+
+            const projectRepository = getRepository(Project);
+            const project = await projectRepository.update(id, req.body);
+
+            return res.status(200).json({ project });
+        } catch (err) {
+            console.log('Error on (update) [project] -> ', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    },
+
+    async delete(req: Request, res: Response) {
+        if(!req.params.id) return res.status(400).json({ error: 'id missing' });
+
+        try {
+            const { id } = req.params;
+
+            const projectRepository = getRepository(Project);
+
+            await projectRepository.delete({ id: Number(id) });
+            const project = await projectRepository.find({ relations: ['images'] });
+
+            return res.status(200).json({ project });
+        } catch (err) {
+            console.log('Error on (delete) [project] -> ', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    },
+};
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import KnowledgeController from './controllers/KnowledgeController';
+import ProjectController from './controllers/ProjectController';
 
 const routes = Router();
 
@@ -10,5 +11,11 @@ routes.get('/knowledge/:id', KnowledgeController.show);
 routes.post('/knowledge', KnowledgeController.create);
 routes.put('/knowledge/:id', KnowledgeController.update);
 routes.delete('/knowledge/:id', KnowledgeController.delete);
+
+routes.get('/project', ProjectController.index);
+routes.get('/project/:id', ProjectController.show);
+routes.post('/project', ProjectController.create);
+routes.put('/project/:id', ProjectController.update);
+routes.delete('/project/:id', ProjectController.delete);
 
 export default routes;
