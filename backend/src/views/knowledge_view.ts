ab387f46ff85205b6febbadb5ce8da1415aceed5
jonasdevzero <jonasdevzero@gmail.com>
2020-11-20 15:59:45 -0300

    feat

---
+++ backend/src/views/knowledge_view.ts
@@ -0,0 +1,19 @@
+import Knowledge from '../models/Knowledge';
+
+export default {
+    render(knowledge: Knowledge) {
+        return {
+            id: knowledge.id,
+            name: knowledge.name,
+            description: knowledge.description,
+            about_link: knowledge.about_link,
+            image_url: knowledge.image_url,
+            type: knowledge.type,
+            lg: knowledge.language,
+        };
+    },
+
+    renderMany(knowledgeArray: Knowledge[]) {
+        return knowledgeArray.map(knowledge => this.render(knowledge));
+    },
+};
---
+++ backend/src/views/projects_view.ts
@@ -0,0 +1,20 @@
+import Project from '../models/Project';
+
+export default {
+    render(project: Project) {
+        return {
+            id: project.id,
+            name: project.name,
+            description: project.description,
+            about_link: project.about_link,
+            source_link: project.source_link,
+            images: project.images,
+            banner_image: project.banner_image,
+            lg: project.language,
+        };
+    },
+
+    renderMany(projects: Project[]) {
+        return projects.map(project => this.render(project));
+    },
+};
