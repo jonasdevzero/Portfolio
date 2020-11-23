1ec012fd6a7e4a667a632f701d62cb1ea2a6ac62
jonasdevzero <jonasdevzero@gmail.com>
2020-11-23 16:56:03 -0300

    feat: starting portfolio page

--- frontend/src/pages/portfolio/index.tsx
+++ frontend/src/pages/portfolio/index.tsx
@@ -1,6 +1,8 @@
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
+import api from '../../services/api'
 
-import { Header, LanguageSelect } from '../../components'
+import { Header, LanguageSelect, Loading } from '../../components'
 
 import {
   Container,
@@ -8,7 +10,34 @@ import {
   Inner
 } from '../../styles/pages/Portfolio'
 
+interface IProjects {
+  id: number;
+  name: string;
+  description: string;
+  objective: string;
+  difficulties: string;
+  acquirements: string;
+  banner_image: string;
+  banner_gif: string;
+  code_link: string;
+  website_link: string;
+  images: Array<{
+    id: number;
+    path: string;
+  }>
+}
+
 function Portfolio() {
+  const [projects, setProjects] = useState<IProjects[]>()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    api.get('/projects?lg=en').then(({ data }) => {
+      setProjects(data.project)
+      setLoading(false)
+    })
+  }, [])
+
   return (
     <div>
       <Head>
@@ -23,6 +52,8 @@ function Portfolio() {
             <h1>Portfolio, more soon...</h1>
           </Inner>
         </Content>
+
+        <Loading loading={loading} />
 
         <LanguageSelect to='/portfolio' />
       </Container>
---
+++ frontend/src/pages/portfolio/projects/[id].tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import api from '../../../services/api'
+
+import { Header, Loading } from '../../../components'
+
+interface IProject {
+  id: number;
+  name: string;
+  description: string;
+  objective: string;
+  difficulties: string;
+  acquirements: string;
+  banner_image: string;
+  banner_gif: string;
+  code_link: string;
+  website_link: string;
+  images: Array<{
+    id: number;
+    path: string;
+  }>
+}
+
+function Portfolio() {
+  const { query } = useRouter()
+  const { id } = query;
+  
+  const [project, setProject] = useState<IProject>()
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    if (id) {
+      api.get(`/projects/${id}`).then(({ data }) => {
+        setProject(data.project)
+        setLoading(false)
+      })
+    }
+  }, [id])
+
+  return (
+    <div>
+      <Head>
+        <title>Project</title>
+      </Head>
+
+      <div>
+        <Header local='portfolio' />
+
+        <div>
+          <h1>{project?.name}</h1>
+          <h2>{project?.description}</h2>
+          <p>{project?.objective}</p>
+        </div>
+
+
+        <Loading loading={loading} />
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
