31a3e40f6d884c58a459a4d9d53c70ee34bb2a52
jonasdevzero <jonasdevzero@gmail.com>
2020-12-01 22:29:12 -0300

    feat: adding project page v1.0

--- frontend/src/pages/portfolio/projects/[id].tsx
+++ frontend/src/pages/portfolio/projects/[id].tsx
@@ -3,11 +3,14 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import api from '../../../services/api'
 import { Header, Loading, LanguageSelect, Slider } from '../../../components'
+import Aos from 'aos'
 
 import {
   Container,
   Content,
-  SlideContainer
+  SlideContainer,
+  InfoContainer,
+  Info,
 } from '../../../styles/pages/projects'
 
 interface IProject {
@@ -28,7 +31,7 @@ interface IProjectImages {
 function Portfolio() {
   const { query } = useRouter()
   const { id } = query;
-  
+
   const [project, setProject] = useState<IProject>()
   const [projectImages, setProjectImages] = useState<IProjectImages[]>()
   const [loading, setLoading] = useState(true)
@@ -37,8 +40,8 @@ function Portfolio() {
     if (id) {
       api.get(`/projects/${id}`).then(({ data }) => {
         setProject(data.project)
-        
-        const images: Array<{url: string}> = []
+
+        const images: Array<{ url: string }> = []
         images.push({ url: data.project.banner_image })
         images.push({ url: data.project.banner_gif })
         data.project.images.map((image: { path: string }) => images.push({ url: image.path }))
@@ -47,6 +50,8 @@ function Portfolio() {
         setLoading(false)
       })
     }
+
+    Aos.init({ duration: 800 })
   }, [id])
 
   return (
@@ -62,20 +67,46 @@ function Portfolio() {
           <SlideContainer>
             <Slider>
               {projectImages?.map((image, i) => (
-                  <img key={i} src={image.url} alt="project image"/>
+                <img key={i} src={image.url} alt="project image" />
               ))}
             </Slider>
           </SlideContainer>
 
-          <h1>{project?.name}</h1>
+          <InfoContainer data-aos='fade-up'>
+            <Info>
+              <h1>{project?.name}</h1>
+              <p>{project?.description}</p>
+
+              <div>
+                <a href={project?.website_link} rel="noopener noreferrer" target="_blank">Website link</a>
+                <a href={project?.code_link} rel="noopener noreferrer" target="_blank">Source Code</a>
+              </div>
+            </Info>
+
+            <Info>
+              <h2>Objective</h2>
+              <p>{project?.objective}</p>
+            </Info>
+
+            <Info>
+              <h2>Difficulties</h2>
+              <p>{project?.difficulties}</p>
+            </Info>
+
+            <Info>
+              <h2>Acquirements</h2>
+              <p>{project?.acquirements}</p>
+            </Info>
+          </InfoContainer>
+
         </Content>
 
 
-        <Loading loading={loading} />
+      <Loading loading={loading} />
 
-        <LanguageSelect to={`/br/portfolio/projects/${Number(id) + 1}`} />
+      <LanguageSelect to={`/br/portfolio/projects/${Number(id) + 1}`} />
       </Container>
-    </div>
+    </div >
   )
 }
 
--- frontend/src/styles/pages/projects.ts
+++ frontend/src/styles/pages/projects.ts
@@ -14,20 +14,83 @@ export const Content = styled.main`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
     flex: 1;
 
     width: 100vw;
-    margin-top: 6rem;
-    margin-bottom: 6rem;
+`;
+
+export const InfoContainer = styled.section`
+    width: 65rem;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    background-color: #191818;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    border-radius: .2rem;
+
+    @media (max-width: 650px) {
+        width: 40rem;
+    };
+    @media (max-width: 400px) {
+        width: 100%;
+        margin: 0;
+    };
 `;
 
 export const SlideContainer = styled.div`
-    width: 65rem;
-    height: 36.6rem;
-    margin-top: 5rem;
+    width: 100%;
+    height: 100vh;
 
     img {
         width: 100%;
         object-fit: contain;
     };
+
+    @media (max-width: 1000px) {
+        height: 37rem;
+    };
+    @media (max-width: 650px) {
+        height: 22rem;
+    };
 `;
+
+export const Info = styled.div`
+    width: 100%;
+    margin-top: 3rem;
+    padding: 3rem;
+    padding-top: 0;
+
+    h1 {
+        font-size: 3.8rem;
+    };
+    h2 {
+        font-size: 2.5rem;
+        margin-bottom: 1.5rem;
+    };
+    p {
+        font-size: 1.7rem;
+        line-height: 2.3rem;
+        text-align: justify;
+    };
+    div {
+        margin-top: 2rem;
+
+        a {
+            color: #777;
+            font-size: 1.6rem;
+            font-weight: 500;
+            text-decoration: none;
+            margin-right: 1rem;
+            transition: color .3s ease;
+
+            &:hover {
+                color: #444;
+            };
+        };
+    };
+`; 
